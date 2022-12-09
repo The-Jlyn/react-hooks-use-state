abecc0d71e6c664cfb6b92d1830a7599601e3de4
The-Jlyn <99376158+The-Jlyn@users.noreply.github.com>
2022-12-09 02:30:35 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  console.log('Loading App comopnent....');
+  // const [count1, setCount1] = useState(0);
+  const arr = useState(4);
+  const [count, setCount] = useState(4)
+  /** 
+   *  useState always returns an array, but we desctructure it for better readability
+   *  - Initializing a state variable
+   * const arr = useState(4);
+   *  const [count, setCount] = useState(4);
+   * 
+   * - Initializing a state variable using function
+   * 
+   * Blog witht he same concepts:
+   * https://blog.webdevsimplified.com/2020-04/use-state/
+   * 
+  */
+
+  const decrementCount = () => {
+    // TODO: Decrement count by 1. 
+    count = setCount(count-1);
+  }
+
+  const incrementCount = () => {
+    // TODO: Increse count by 1. 
+    count = setCount(count+1);
+  }
+  return (
+    <div className="App">
+      <button onClick={decrementCount}>-</button>
+      <h1>{count}</h1>
+      <button onClick={incrementCount}>+</button>
+    </div>
+  );
+}
+
+export default App;
